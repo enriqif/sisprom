6c5bdaf265d7823fbc23e87d6428cfdb8785e3ec
carrizofm2010@gmail.com <carrizofm2010@gmail.com@086a9d37-5281-2dbd-de61-b035e7501a1e>
2014-09-02 21:38:57 +0000

    CLases pruebas xml

---
+++ sisprom/src/main/java/com/sisprom/framework/model/xml/hibernate/util/HibernateUtil.java
@@ -0,0 +1,36 @@
+package com.sisprom.framework.model.xml.hibernate.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+/**
+ * 
+ * @author José
+ *
+ */
+public class HibernateUtil {
+    private SessionFactory sessionFactory;
+    private Session session;
+
+    public HibernateUtil()
+    {
+      sessionFactory = new Configuration().configure("com/sisprom/framework/model/xml/config/hibernate.cfg.xml").buildSessionFactory();
+      session = sessionFactory.openSession();
+    }
+
+    public Session getSession() {
+        return session;
+    }
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+}
