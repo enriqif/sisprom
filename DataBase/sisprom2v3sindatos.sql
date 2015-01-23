4509a4557f23fea9a06990e3e19961679deb5916
alcantara.marcos00@gmail.com <alcantara.marcos00@gmail.com@086a9d37-5281-2dbd-de61-b035e7501a1e>
2015-01-23 15:36:04 +0000

    modificacion de bs sin datos

--- DataBase/sisprom2v3sindatos.sql
+++ DataBase/sisprom2v3sindatos.sql
@@ -2,6 +2,71 @@
 -- Archivo creado  - viernes-enero-23-2015   
 --------------------------------------------------------
 --------------------------------------------------------
+--  DDL for Sequence ADMINISTRADOR_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ADMINISTRADOR_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence ANTECEDENTE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "ANTECEDENTE_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence CONSULTA_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "CONSULTA_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence DIAGNOSTICO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "DIAGNOSTICO_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence HISTORIA_CLINICA_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "HISTORIA_CLINICA_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence MEDICAMENTO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "MEDICAMENTO_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence MEDICO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "MEDICO_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence PACIENTE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "PACIENTE_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence SECRETARIA_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SECRETARIA_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence TIPO_ANTECEDENTE_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TIPO_ANTECEDENTE_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence TRATAMIENTO_MEDICAMENTO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TRATAMIENTO_MEDICAMENTO_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence TRATAMIENTO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TRATAMIENTO_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence TURNO_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "TURNO_SEQ"  MINVALUE 1 MAXVALUE 999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
 --  DDL for Table ANTECEDENTE_GENERAL
 --------------------------------------------------------
 
@@ -253,3 +318,176 @@
 	"USUARIO_DOMICILIO_CALLE" VARCHAR2(50), 
 	"USUARIO_DOMICILIO_NUMERO" VARCHAR2(50)
    ) ;
+--------------------------------------------------------
+--  Constraints for Table ANTECEDENTE_GENERAL
+--------------------------------------------------------
+
+  ALTER TABLE "ANTECEDENTE_GENERAL" ADD CONSTRAINT "ANTECEDENTE_GRAL_ID" PRIMARY KEY ("ANTECEDENTE_GRAL_ID") ENABLE;
+ 
+  ALTER TABLE "ANTECEDENTE_GENERAL" MODIFY ("ANTECEDENTE_GRAL_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table ANTECEDENTE_PER_PATOLOGICO
+--------------------------------------------------------
+
+  ALTER TABLE "ANTECEDENTE_PER_PATOLOGICO" ADD CONSTRAINT "ANTECEDENTE_PER_PAT_ID" PRIMARY KEY ("ANTECEDENTE_PER_PAT_ID") ENABLE;
+ 
+  ALTER TABLE "ANTECEDENTE_PER_PATOLOGICO" MODIFY ("ANTECEDENTE_PER_PAT_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table CONSULTA
+--------------------------------------------------------
+
+  ALTER TABLE "CONSULTA" ADD CONSTRAINT "CONSULTA_ID" PRIMARY KEY ("CONSULTA_ID") ENABLE;
+ 
+  ALTER TABLE "CONSULTA" MODIFY ("CONSULTA_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table DETALLE_PATOLOGIA
+--------------------------------------------------------
+
+  ALTER TABLE "DETALLE_PATOLOGIA" ADD CONSTRAINT "DETALLE_PAT_ID" PRIMARY KEY ("DETALLE_PAT_ID") ENABLE;
+ 
+  ALTER TABLE "DETALLE_PATOLOGIA" MODIFY ("DETALLE_PAT_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table DIAGNOSTICO
+--------------------------------------------------------
+
+  ALTER TABLE "DIAGNOSTICO" ADD CONSTRAINT "DIAGNOSTICO_ID" PRIMARY KEY ("DIAGNOSTICO_ID") ENABLE;
+ 
+  ALTER TABLE "DIAGNOSTICO" MODIFY ("DIAGNOSTICO_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table HISTORIA_CLINICA
+--------------------------------------------------------
+
+  ALTER TABLE "HISTORIA_CLINICA" ADD CONSTRAINT "HISTORIA_CLINICA_ID" PRIMARY KEY ("HISTORIA_CLINICA_ID") ENABLE;
+ 
+  ALTER TABLE "HISTORIA_CLINICA" MODIFY ("HISTORIA_CLINICA_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table MEDICAMENTO
+--------------------------------------------------------
+
+  ALTER TABLE "MEDICAMENTO" ADD CONSTRAINT "MEDICAMENTO_ID" PRIMARY KEY ("MEDICAMENTO_ID") ENABLE;
+ 
+  ALTER TABLE "MEDICAMENTO" MODIFY ("MEDICAMENTO_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table PACIENTE
+--------------------------------------------------------
+
+  ALTER TABLE "PACIENTE" ADD CONSTRAINT "PACIENTE_ID" PRIMARY KEY ("PACIENTE_ID") ENABLE;
+ 
+  ALTER TABLE "PACIENTE" MODIFY ("PACIENTE_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table PATOLOGIA
+--------------------------------------------------------
+
+  ALTER TABLE "PATOLOGIA" ADD CONSTRAINT "PATOLOGIA_ID" PRIMARY KEY ("PATOLOGIA_ID") ENABLE;
+ 
+  ALTER TABLE "PATOLOGIA" MODIFY ("PATOLOGIA_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table PERMISO
+--------------------------------------------------------
+
+  ALTER TABLE "PERMISO" ADD CONSTRAINT "PERMISO_ID" PRIMARY KEY ("PERMISO_ID") ENABLE;
+ 
+  ALTER TABLE "PERMISO" MODIFY ("PERMISO_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table TRATAMIENTO
+--------------------------------------------------------
+
+  ALTER TABLE "TRATAMIENTO" MODIFY ("TRATAMIENTO_ID" NOT NULL ENABLE);
+ 
+  ALTER TABLE "TRATAMIENTO" ADD CONSTRAINT "TRATAMIENTO_ID" PRIMARY KEY ("TRATAMIENTO_ID") ENABLE;
+--------------------------------------------------------
+--  Constraints for Table TRATAMIENTO_MEDICAMENTO
+--------------------------------------------------------
+
+  ALTER TABLE "TRATAMIENTO_MEDICAMENTO" MODIFY ("TRATAMIENTO_MEDICAMENTO_ID" NOT NULL ENABLE);
+ 
+  ALTER TABLE "TRATAMIENTO_MEDICAMENTO" ADD CONSTRAINT "TRATAMIENTO_MEDICAMENTO_PK" PRIMARY KEY ("TRATAMIENTO_MEDICAMENTO_ID") ENABLE;
+--------------------------------------------------------
+--  Constraints for Table TURNO
+--------------------------------------------------------
+
+  ALTER TABLE "TURNO" MODIFY ("TURNO_ID" NOT NULL ENABLE);
+ 
+  ALTER TABLE "TURNO" ADD CONSTRAINT "TURNO_ID" PRIMARY KEY ("TURNO_ID") ENABLE;
+--------------------------------------------------------
+--  Constraints for Table USUARIO
+--------------------------------------------------------
+
+  ALTER TABLE "USUARIO" MODIFY ("USUARIO_ID" NOT NULL ENABLE);
+ 
+  ALTER TABLE "USUARIO" MODIFY ("PERMISO_ID" NOT NULL ENABLE);
+ 
+  ALTER TABLE "USUARIO" ADD CONSTRAINT "USUARIO_ID" PRIMARY KEY ("USUARIO_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table ANTECEDENTE_GENERAL
+--------------------------------------------------------
+
+  ALTER TABLE "ANTECEDENTE_GENERAL" ADD CONSTRAINT "ANTECEDENTE_GENERAL_HISTO_FK1" FOREIGN KEY ("HISTORIA_CLINICA_ID")
+	  REFERENCES "HISTORIA_CLINICA" ("HISTORIA_CLINICA_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table ANTECEDENTE_PER_PATOLOGICO
+--------------------------------------------------------
+
+  ALTER TABLE "ANTECEDENTE_PER_PATOLOGICO" ADD CONSTRAINT "ANTECEDENTE_PER_PATOLOGIC_FK1" FOREIGN KEY ("HISTORIA_CLINICA_ID")
+	  REFERENCES "HISTORIA_CLINICA" ("HISTORIA_CLINICA_ID") ENABLE;
+ 
+  ALTER TABLE "ANTECEDENTE_PER_PATOLOGICO" ADD CONSTRAINT "ANTECEDENTE_PER_PATOLOGIC_FK2" FOREIGN KEY ("DETALLE_PAT_ID")
+	  REFERENCES "DETALLE_PATOLOGIA" ("DETALLE_PAT_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table CONSULTA
+--------------------------------------------------------
+
+  ALTER TABLE "CONSULTA" ADD CONSTRAINT "CONSULTA_HISTORIA_CLINICA_FK1" FOREIGN KEY ("HISTORIA_CLINICA_ID")
+	  REFERENCES "HISTORIA_CLINICA" ("HISTORIA_CLINICA_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table DETALLE_PATOLOGIA
+--------------------------------------------------------
+
+  ALTER TABLE "DETALLE_PATOLOGIA" ADD CONSTRAINT "DETALLE_PATOLOGIA_PATOLOG_FK1" FOREIGN KEY ("PATOLOGIA_ID")
+	  REFERENCES "PATOLOGIA" ("PATOLOGIA_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table DIAGNOSTICO
+--------------------------------------------------------
+
+  ALTER TABLE "DIAGNOSTICO" ADD CONSTRAINT "DIAGNOSTICO_CONSULTA_FK1" FOREIGN KEY ("CONSULTA_ID")
+	  REFERENCES "CONSULTA" ("CONSULTA_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table HISTORIA_CLINICA
+--------------------------------------------------------
+
+  ALTER TABLE "HISTORIA_CLINICA" ADD CONSTRAINT "HISTORIA_CLINICA_PACIENTE_FK1" FOREIGN KEY ("PACIENTE_ID")
+	  REFERENCES "PACIENTE" ("PACIENTE_ID") ENABLE;
+ 
+  ALTER TABLE "HISTORIA_CLINICA" ADD CONSTRAINT "HISTORIA_CLINICA_USUARIO_FK1" FOREIGN KEY ("USUARIO_ID")
+	  REFERENCES "USUARIO" ("USUARIO_ID") ENABLE;
+
+
+
+
+--------------------------------------------------------
+--  Ref Constraints for Table TRATAMIENTO
+--------------------------------------------------------
+
+  ALTER TABLE "TRATAMIENTO" ADD CONSTRAINT "TRATAMIENTO_DIAGNOSTICO_FK1" FOREIGN KEY ("DIAGNOSTICO_ID")
+	  REFERENCES "DIAGNOSTICO" ("DIAGNOSTICO_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table TRATAMIENTO_MEDICAMENTO
+--------------------------------------------------------
+
+  ALTER TABLE "TRATAMIENTO_MEDICAMENTO" ADD CONSTRAINT "TRATAMIENTO_MEDICAMENTO_M_FK1" FOREIGN KEY ("MEDICAMENTO_ID")
+	  REFERENCES "MEDICAMENTO" ("MEDICAMENTO_ID") ENABLE;
+ 
+  ALTER TABLE "TRATAMIENTO_MEDICAMENTO" ADD CONSTRAINT "TRATAMIENTO_MEDICAMENTO_T_FK1" FOREIGN KEY ("TRATAMIENTO_ID")
+	  REFERENCES "TRATAMIENTO" ("TRATAMIENTO_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table TURNO
+--------------------------------------------------------
+
+  ALTER TABLE "TURNO" ADD CONSTRAINT "TURNO_PACIENTE_FK1" FOREIGN KEY ("PACIENTE_ID")
+	  REFERENCES "PACIENTE" ("PACIENTE_ID") ENABLE;
+--------------------------------------------------------
+--  Ref Constraints for Table USUARIO
+--------------------------------------------------------
+
+  ALTER TABLE "USUARIO" ADD CONSTRAINT "USUARIO_PERMISO_FK1" FOREIGN KEY ("PERMISO_ID")
+	  REFERENCES "PERMISO" ("PERMISO_ID") ENABLE;
