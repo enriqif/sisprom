2cd0619c3d9143f4257c1c7b6ed441a78865faf3
carrizofm2010 <carrizofm2010@gmail.com>
2015-04-01 11:54:48 -0300

    bean de personal y guardar nuevo personal

--- nuevoSisprom2/sisprom/src/main/java/com/sisprom/framework/managedBean/PersonalManagedBean.java
+++ nuevoSisprom2/sisprom/src/main/java/com/sisprom/framework/managedBean/PersonalManagedBean.java
@@ -5,61 +5,44 @@ import java.util.Date;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import com.sisprom.framework.dominio.Permiso;
+import com.sisprom.framework.dominio.Usuario;
+
 
 @ManagedBean
 @SessionScoped
 public class PersonalManagedBean extends MasterManagedBean {
 
-	private String nombre;
-	private String apellido;
-	private String direccion;
-	private String telefono;
-	private String dni;
-	private Date horarioAtencionMin;
-	private Date horarioAtencionMax;
-	
-	public Date getHorarioAtencionMin() {
-		return horarioAtencionMin;
+	private Usuario usuario = new Usuario();
+	private Permiso permiso = new Permiso();
+
+	public String nuevo(){
+		try {
+			permiso.setPermisoId(3);
+			usuario.setFechaCreacion(null);
+			usuario.setFechaModificacion(null);
+			usuario.setUsuarioHoraAtencionMax(null);
+			usuario.setUsuarioHoraAtencionMin(null);
+			usuario.setUsuarioEspecialidad("Nulo");
+			usuario.setUsuarioMatricula("Nulo");
+			usuario.setUsuarioRol("Personal");
+			usuario.setPermiso(permiso);
+			super.getServices().saveUsuario(usuario);
+			
+			return "hecho";			
+		} catch (Exception e) {
+			//TODO
+			//Se debe definir la vista cuando se produce un error
+			return "errorGuardado";
+		}
 	}
-	public void setHorarioAtencionMin(Date horarioAtencionMin) {
-		this.horarioAtencionMin = horarioAtencionMin;
+	public Usuario getUsuario() {
+		return usuario;
 	}
-	public Date getHorarioAtencionMax() {
-		return horarioAtencionMax;
-	}
-	public void setHorarioAtencionMax(Date horarioAtencionMax) {
-		this.horarioAtencionMax = horarioAtencionMax;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getDireccion() {
-		return direccion;
-	}
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-	public String getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-	public String getDni() {
-		return dni;
-	}
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	} 
 	
 	
 }
